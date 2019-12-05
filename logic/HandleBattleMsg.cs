8d070884878754b26b0de92f0edf5fe8fe00645e
Rainer <782174139@qq.com>
2019-12-06 00:23:38 +0800

    net

--- logic/HandleBattleMsg.cs
+++ logic/HandleBattleMsg.cs
@@ -41,4 +41,43 @@ public partial class HandlePlayerMsg
         player.Send(protocol);
         room.StartFight();
     }
+
+    /// <summary>
+    /// 同步坦克单元
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="protoBase"></param>
+    public void MsgUpdateUnitInfo(Player player, ProtocolBase protoBase)
+    {
+        ProtocolBytes protocol = (ProtocolBytes)protoBase;
+        int start = 0;
+        string protoName = protocol.GetString(start, ref start);
+        float posX = protocol.GetFloat(start, ref start);
+        float posY = protocol.GetFloat(start, ref start);
+        float posZ = protocol.GetFloat(start, ref start);
+        float rotX = protocol.GetFloat(start, ref start);
+        float rotY = protocol.GetFloat(start, ref start);
+        float rotZ = protocol.GetFloat(start, ref start);
+        float gunRot = protocol.GetFloat(start, ref start);
+        float gunRoll = protocol.GetFloat(start, ref start);
+        if (player.tempData.status != PlayerTempData.Status.Fight)
+            return;
+        Room room = player.tempData.room;
+        player.tempData.posX = posX;
+        player.tempData.posY = posY;
+        player.tempData.posZ = posZ;
+        player.tempData.lastUpdateTime = Sys.GetTimeStamp();
+        ProtocolBytes protoRet = new ProtocolBytes();
+        protoRet.AddString("UpdateUnitInfo");
+        protoRet.AddString(player.id);
+        protoRet.AddFloat(posX);
+        protoRet.AddFloat(posY);
+        protoRet.AddFloat(posZ);
+        protoRet.AddFloat(rotX);
+        protoRet.AddFloat(rotY);
+        protoRet.AddFloat(rotZ);
+        protoRet.AddFloat(gunRot);
+        protoRet.AddFloat(gunRoll);
+        room.Broadcast(protoRet);
+    }
 }
